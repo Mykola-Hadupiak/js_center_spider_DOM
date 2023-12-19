1f01f8ec1bfceb857d563b308f58f2b0fd0c09b1
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-12-20 00:07:38 +0200

    make build

---
+++ index.b1f07dfc.js
@@ -0,0 +1,2 @@
+function e(e,t,i){var r=document.querySelector(t),c=document.querySelector(i),l=r["height"===e?"clientHeight":"clientWidth"],n=c.getBoundingClientRect()[e];return"".concat(l/2-n/2,"px")}var t=document.querySelector(".spider").style;t.left=e("width",".wall",".spider"),t.top=e("height",".wall",".spider");
+//# sourceMappingURL=index.b1f07dfc.js.map
